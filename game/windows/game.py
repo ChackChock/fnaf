9b7e5df2d05d546ddf675396dac24dee3a6195bb
Burkin Nikita <92577296+ChackChock@users.noreply.github.com>
2024-03-21 20:03:27 +0300

    Add files via upload

--- game/windows/game.py
+++ game/windows/game.py
@@ -1,5 +1,6 @@
 import pygame
 
+from scripts import gui
 from scripts.system import display
 
 from .. import network
@@ -10,6 +11,14 @@ from .base_window import BaseWindow
 class GameWindow(BaseWindow):
     name = "game"
 
+    def _build(self) -> gui.Container:
+        main_layout = gui.VerticalLayout(display.get_surface_center())
+        self.__label = gui.Label(0, "", "main")
+
+        main_layout.add(self.__label)
+
+        return main_layout
+
     def load(self) -> None:
         super().load()
         display.set_clear_color((255, 255, 255))
@@ -18,3 +27,20 @@ class GameWindow(BaseWindow):
 
     def update(self) -> None:
         super().update()
+
+        for data in network.get_data():
+            if data["key"] == "deleting":
+                self.__label.set_text("")
+            elif data["data"] == "whitespace":
+                self.__label.set_text(self.__label.get_text() + "\n")
+            else:
+                self.__label.set_text(self.__label.get_text() + data["data"])
+
+    def process_event(self, event: pygame.Event) -> None:
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_BACKSPACE:
+                network.send_message("deleting", None)
+            elif event.key in (pygame.K_KP_ENTER, pygame.KSCAN_KP_ENTER):
+                network.send_message("typing", "whitespace")
+            else:
+                network.send_message("typing", event.unicode)
